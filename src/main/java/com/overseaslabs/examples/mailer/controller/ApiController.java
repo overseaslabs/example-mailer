c5ab2fc28b4b841d39bb5c25e880119ae77d037b
Sergey Vaschenko <vaschenko.sergey@gmail.com>
2018-09-10 09:36:51 +0300

    throw an exception from the controller

--- src/main/java/com/overseaslabs/examples/mailer/controller/ApiController.java
+++ src/main/java/com/overseaslabs/examples/mailer/controller/ApiController.java
@@ -2,12 +2,14 @@ package com.overseaslabs.examples.mailer.controller;
 
 import com.overseaslabs.examples.mailer.entity.Email;
 import com.overseaslabs.examples.mailer.repository.EmailRepository;
+import com.overseaslabs.examples.utils.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Optional;
 
 /**
@@ -26,8 +28,8 @@ public class ApiController {
      * @return Email instance
      */
     @GetMapping("/emails/{id}")
-    public Optional<Email> get(@PathVariable int id) {
-        return emailRepository.findById(id);
+    public Email get(@PathVariable int id) throws ResourceNotFoundException {
+        return emailRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Email " + id + " not found"));
     }
 
     /**
